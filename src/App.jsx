501e3fa156dc749010bfb89a3a9ce4f0a5100c83
Bernardo Antunes Heckler <bernaheckler1@gmail.com>
2025-05-05 10:30:13 -0300

    feat: ajuste de deploy.

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import "./App.css";
+import Caixa from "./pages/Caixa";
+
+function App() {
+  return (
+    <>
+      <div className="Container">
+        <Caixa />
+      </div>
+    </>
+  );
+}
+
+export default App;
